db613dcca3337dd010f047beb41d95daacb914d1
Cristiane Silva <cristianeavelarmaia@gmail.com>
2018-06-11 18:39:41 +0000

    Step 1 completed

---
+++ step-1.js
@@ -0,0 +1,29 @@
+
+
+function getAndPrintHTMLChunks () {
+  var https = require('https');
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function(response) {
+
+    //set encoding to UTF-8
+    response.setEncoding('utf8');
+
+    //invoked when data is received
+    response.on('data', function (data) {
+      console.log('Chunk of data received. Data: ', data + "\n");
+    });
+
+    //callback for when all data has been received
+    response.on('end', function() {
+      console.log('Response stream complete');
+    });
+
+  });
+
+}
+
+ getAndPrintHTMLChunks();
