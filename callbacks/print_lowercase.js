eab77eabd31d02d947c9f650bb75b8d3ad3197f3
Cristiane Silva <cristianeavelarmaia@gmail.com>
2018-06-11 20:32:36 +0000

    Print lowercase working

--- callbacks/print_lowercase.js
+++ callbacks/print_lowercase.js
@@ -1,1 +1,13 @@
-var lowercase = require('./http-functions');
+var getHTML = require('./http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/lowercase.html'
+};
+
+function printLowerCase(html) {
+
+  console.log(html.toLowerCase());
+}
+
+getHTML(requestOptions, printLowerCase);
